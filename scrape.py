22a5789e33600dc0ab95cc520cf3078d16b27b57
Baki <bakiguher@hotmail.com>
2022-02-28 18:08:58 +0100

    property get data finished

--- scrape.py
+++ scrape.py
@@ -17,7 +17,6 @@ class HouseData:
     number_of_rooms = str  # int
     area = str  # float
     fully_equipped_kitchen = str  # yes no (no data) int
-    furnished = str  # int yes no no data
     open_fire = str
     terrace = str
     terrace_area = str
@@ -26,8 +25,8 @@ class HouseData:
     surface_land = str
     surface_plot_land = str
     number_of_facades = str
-    swimmin_pool = str
-    state_of_building = str
+    swimming_pool = str
+    isnewly_built = str
 
 
 ur = 'https://www.immoweb.be/en/classified/house/for-sale/merelbeke/9820/9768055?searchId=621c996e4790f'
@@ -64,8 +63,16 @@ def scrape_house1(url):
     h_data.number_of_rooms = data["property"]["bedroomCount"]
     h_data.area = data["property"]["land"]["surface"]
     h_data.fully_equipped_kitchen = data["property"]["kitchen"]["type"]
-
-
+    h_data.open_fire = data["property"]["fireplaceExists"]
+    h_data.terrace = data["property"]["hasTerrace"]
+    h_data.terrace_area = data["property"]["terraceSurface"]
+    h_data.garden = data["property"]["hasGarden"]
+    h_data.garden_area = data["property"]["gardenSurface"]
+    h_data.surface_land = data["property"]["land"]["surface"]
+    h_data.surface_plot_land = data["property"]["netHabitableSurface"]
+    h_data.number_of_facades = data["property"]["building"]["facadeCount"]
+    h_data.swimming_pool = data["property"]["hasSwimmingPool"]
+    h_data.isnewly_built = data["flags"]["isNewlyBuilt"]
 
 
 
